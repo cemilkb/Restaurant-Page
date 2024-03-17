97906ce14f6c1c9ef7f1ad75ba20d051c6b91d48
cemilkb <cemilkoybasi@hotmail.com>
2024-03-17 16:24:21 +0300

    sayfa js ler oluşturuldu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import './input.css'
 import '../dist/output.css'
 // import Bu from './assets/kul.png' img çekmek için
-console.log("DENEME")
-let a = document.getElementById("a")
-a.textContent = "saşldk"
-a.style.color = "red"
