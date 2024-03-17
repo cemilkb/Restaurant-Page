6bde4d3bd26890246d96186113f1d3e6cb1fac2d
cemilkb <cemilkoybasi@hotmail.com>
2024-03-17 16:13:43 +0300

    hotfix

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './input.css'
 import '../dist/output.css'
 // import Bu from './assets/kul.png' img çekmek için
 console.log("DENEME")
+let a = document.getElementById("a")
+a.textContent = "saşldk"
+a.style.color = "red"
