6f787528e87b086a74802d7cc6def2f582dacee5
cemilkb <cemilkoybasi@hotmail.com>
2024-03-18 15:09:00 +0300

    add button click feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './input.css'
 import '../dist/output.css'
 // import Bu from './assets/kul.png' img çekmek için
+
+const div = document.getElementById("content")
+const navs = document.querySelectorAll("nav")
+
+navs.forEach((e) => {
+    e.addEventListener("click", () => {
+        navs.forEach(e => {
+            e.classList.remove("clicked")
+        })
+        e.classList.add("clicked")
+    })
+})
+
+div.textContent = "ANANZA"
